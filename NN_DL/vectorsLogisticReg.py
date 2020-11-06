5c6ba79570658fca10640d0942ad3538350c2ce7
malithjkd <malithjayawardhana@gmail.com>
2020-11-06 08:15:15 +0530

    vectorisation of logistic regression algorithem is done

--- NN_DL/vectorsLogisticReg.py
+++ NN_DL/vectorsLogisticReg.py
@@ -3,18 +3,31 @@ import numpy as np
 #z = np.zeros((10,),dtype=int)
 #print(z)
 
-b = -0.01
-
+b = 0
 #print(b)
+
 X = np.random.randint(2,size=(10,10))
+Y = np.random.randint(2,size=(1,10))
+#print(X)
+#print(Y)
 
 W = np.random.rand(10)
 WT = W.transpose()
 #print(WT)
 
-z = np.dot(WT,X)+b
-z = np.round_(z,decimals=3)
+Z = np.dot(WT,X)+b
+#Z = np.round_(Z,decimals=3)
 #print(z)
 
-a = 1/(1+np.exp(-1*z))
-print(np.round_(a,decimals=3))
+A = 1/(1+np.exp(-1*Z))
+#print(np.round_(A,decimals=3))
+
+dZ = A-Y
+#print(dZ)
+
+dZT = dZ.transpose()
+dW = (1/10)*np.dot(X,dZT)
+dB = (1/10)*np.sum(dZ)
+print(dW)
+print(dB)
+
